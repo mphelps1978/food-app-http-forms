df877af959550b85e4979f5ecb26656f536b594c
Michael Phelps <michaeljphelps1978@gmail.com>
2022-01-15 19:59:14 +0000

    Moves meals to FireBase DB, implements fetch and adds loading state

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-practice-4cba1-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setLoading(false);
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  if (loading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading..</p>
+      </section>
+    );
+  }
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
